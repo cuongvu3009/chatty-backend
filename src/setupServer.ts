c34e517ab8700c0bb0dbee10cbcdfe5dff8b8bc8
cuongvu3009 <cuongvu3009@gmail.com>
2023-02-24 00:59:20 +0200

    feat: add standardMiddleware & securityMiddleware

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,5 +1,12 @@
-import { Application } from 'express';
+import { Application, json, urlencoded } from 'express';
 import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import compression from 'compression';
+import 'express-async-errors';
 
 export class ChattyServer {
   private app: Application;
@@ -16,9 +23,32 @@ export class ChattyServer {
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: ['key1', 'keys'],
+        maxAge: 24 * 7 * 3600000,
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: '*',
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      })
+    );
+  }
 
-  private standardMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
 
   private routesMiddleware(app: Application): void {}
 
